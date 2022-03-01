5f7d67f3850a9e9e939ab2bbdbee65efa3813ce2
TomoFern <tomasfernandez210@gmail.com>
2022-03-01 14:16:06 +0000

    Created GUI

---
+++ Code/GUI.py
@@ -0,0 +1,12 @@
+#These 2 lines of code import the Tkinter Library which allows me to create a GUI
+from tkinter import *
+from tkinter import ttk
+import LoginValidation
+
+app = Tk()
+frm = ttk.Frame(app, padding = 10)
+frm.grid()
+app.title("AniCare")
+app.geometry('500x500')
+app['bg'] = '#de6969'
+app.mainloop()
---
+++ Code/LoginValidation.py
@@ -0,0 +1,4 @@
+import csv
+usernames = "usernames.csv"
+with open(usernames, newline='')as csvfile:
+    print()
