0a121d7f4c19343ce8b9869393e579fd9a183a97
TomoFern <tomasfernandez210@gmail.com>
2022-03-16 11:54:48 +0000

    ITS WORKING

--- Code/GUI.py
+++ Code/GUI.py
@@ -1,4 +1,5 @@
 #These 2 lines of code import the Tkinter Library which allows me to create a GUI
+from textwrap import shorten
 from tkinter import *
 from tkinter import ttk
 import LoginValidation
@@ -7,10 +8,41 @@ app = Tk()
 frm = ttk.Frame(app, padding = 10)
 frm.grid()
 app.title("AniCare")
-app.geometry('500x500')
+app.geometry('1920x1080')
 app['bg'] = '#de6969'
-userentry = Entry(app, bg = 'white', bd=5)
-userentry.place(relx=0.5, rely=0.5, anchor=CENTER)
-testvar = (userentry.get())
+
+userstore = StringVar()
+passstore = StringVar()
+
+def Checker():
+     username = LoginValidation.UsernameValidator(userstore.get())
+     if username == -1:
+          print("Invalid Username")
+     else:
+          password = LoginValidation.PasswordValidator(passstore.get(), username)
+          if password == -1:
+               print("Invalid Password")
+          else:
+               print("Access Granted")
+
+box = Label(text="")
+box.config(height = 50, width = 100)
+box.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+usertext = Label(text = "Username")
+usertext.place(relx = 0.5, rely = 0.275, anchor = CENTER)
+userentry = Entry(app, bg = 'white', bd = 5, textvariable = userstore)
+userentry.config(width = 30)
+userentry.focus()
+userentry.place(relx = 0.5, rely = 0.3, anchor = CENTER)
+
+passtext = Label(text = "Password")
+passtext.place(relx = 0.5, rely = 0.375, anchor = CENTER)
+passentry = Entry(app, bg = 'white', bd = 5, textvariable = passstore, show = "*")
+passentry.config(width = 30)
+passentry.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+
+enterbutton = Button(text = "Enter", command = Checker)
+enterbutton.place(relx = 0.5, rely = 0.6, anchor = CENTER)
 
 app.mainloop()
--- Code/LoginValidation.py
+++ Code/LoginValidation.py
@@ -1,47 +1,57 @@
 #Imports the csv library allowing me to open and read and write to a csv file
 import csv
 
-usernames = r"c:\Users\stu-fernandez.t\OneDrive - Brighter Futures Learning Partnership Trust\Documents\GitHub\AniCare\Code\usernames.csv"
+#These lines of code open the usernames csv file and read the items into an array called userarray
+usernames = r'usernames.csv'
 with open(usernames)as userfile:
     userreader = csv.reader(userfile, skipinitialspace=True, delimiter = ',')
     userarray = list(userreader)
 
-passwords = r"c:\Users\stu-fernandez.t\OneDrive - Brighter Futures Learning Partnership Trust\Documents\GitHub\AniCare\Code\passwords.csv"
+#These lines of code open the passwords csv file and read the items into an array called passarray
+passwords = r"passwords.csv"
 with open(passwords)as passfile:
     passreader = csv.reader(passfile, skipinitialspace=True, delimiter = ',')
     passarray = list(passreader)
 
-print(userfile)
-print(userarray)
-print(passarray)
 
-
+#The UsernameValidator function checks a given username to see if it exists in userarray
+#It then returns the position of the username in the array if found or says invalid username if not found
 def UsernameValidator(userEnter):
     maxRange = (len(userarray[0]))
     for i in range(0, maxRange):
         if userEnter == userarray[0][i]:
-            print("test")
             return (i)
         elif i == (maxRange - 1):
-            return("Invalid Username")
+            return(-1)
         else:
             i += 1
 
+#The PasswordValidator function checks to see if the given password is the same as the password stored at the same
+# position in the array as the username
+#If it matches, access is granted to the application, if not, it says invalid password
 def PasswordValidator(passEnter, passPos):
     if passEnter == passarray[0][passPos]:
-        print("Valid Password")
         return(1)
     else:
-        print("Invalid Password")
         return(-1)
 
+
+"""
+#These 2 print statements are testing that the files were read correctly
+print(userarray)
+print(passarray)
+
+#These 2 temporary inputs took the place of an input box on the GUI
 temp = input("What is your username? ")
 temp2 = input("What is your password? ")
 
+#Here the UsernameValidator function is called in the variable userPostion which will store the returned value
 userPosition = UsernameValidator(temp)
 
-if userPosition == "Invalid Username":
-    print(userPosition)
+#This if statement runs the PasswordValidator function if the username given was valid, if not, it doesn't run the function
+if userPosition == -1:
+    print()
 else:
     checkAccess = PasswordValidator(temp2, userPosition)
+"""
 
